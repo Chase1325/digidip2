2da15cd23d66f9fbb56806ebf43cff77ba0db52d
ChaseStLaurent <clstlaurent@wpi.edu>
2016-05-23 14:18:40 -0400

    MakeshiftArduino.ino file

---
+++ MakeShiftArduino/MakeShiftArduino/MakeShiftArduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MakeShiftArduino.ino
+ Created:	5/23/2016 2:17:56 PM
+ Author:	ChaseStLaurent
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
